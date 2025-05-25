a75e7217a22de26c2bebbd9f1b8d7c41eb9e872e
Maite <anamaitegarciacorrea@gmail.com>
2025-05-26 00:15:00 +0200

    Actualización bd

---
+++ copiaBD/proyecto (4).sql
@@ -0,0 +1,206 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 26-05-2025 a las 00:14:11
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyecto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentario`
+--
+
+CREATE TABLE `comentario` (
+  `id` bigint(11) NOT NULL,
+  `contenido` varchar(350) NOT NULL,
+  `usuario` bigint(11) UNSIGNED NOT NULL,
+  `post` int(11) UNSIGNED NOT NULL,
+  `fecha` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `comentario`
+--
+
+INSERT INTO `comentario` (`id`, `contenido`, `usuario`, `post`, `fecha`) VALUES
+(4, 'dewSF', 1, 21, '2025-05-26 00:11:25');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `likes`
+--
+
+CREATE TABLE `likes` (
+  `id` int(11) NOT NULL,
+  `usuario` bigint(11) UNSIGNED NOT NULL,
+  `post` int(11) UNSIGNED NOT NULL,
+  `fecha` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `post`
+--
+
+CREATE TABLE `post` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `usuario` bigint(11) UNSIGNED NOT NULL,
+  `contenido` varchar(500) NOT NULL,
+  `fecha` datetime NOT NULL,
+  `tags` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `post`
+--
+
+INSERT INTO `post` (`id`, `usuario`, `contenido`, `fecha`, `tags`) VALUES
+(11, 2, 'hola mundo', '2025-05-25 18:19:05', ''),
+(12, 2, 'dewfnawuh', '2025-05-25 18:19:21', ''),
+(13, 2, 'dewFW', '2025-05-25 18:28:35', ''),
+(14, 2, 'vdvf', '2025-05-25 18:29:45', ''),
+(15, 2, 'vdstbst', '2025-05-25 18:37:05', ''),
+(16, 2, 'hdbashebdakwj', '2025-05-25 18:43:33', ''),
+(17, 2, 'cascd', '2025-05-25 18:47:15', ''),
+(18, 2, 'dscwsc', '2025-05-25 18:49:54', ''),
+(19, 2, 'frasgerg', '2025-05-25 19:09:26', ''),
+(20, 2, 'heyyy', '2025-05-25 19:09:33', ''),
+(21, 2, 'csr', '2025-05-25 19:10:02', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` bigint(11) UNSIGNED NOT NULL,
+  `telefono` int(9) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `nickname` varchar(20) NOT NULL,
+  `descripcion` varchar(850) DEFAULT NULL,
+  `nacimiento` date DEFAULT NULL,
+  `passwrd` varchar(80) NOT NULL,
+  `img` varchar(200) DEFAULT NULL,
+  `tags` varchar(400) DEFAULT NULL,
+  `tipo` varchar(5) NOT NULL DEFAULT 'usu'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `telefono`, `email`, `nombre`, `nickname`, `descripcion`, `nacimiento`, `passwrd`, `img`, `tags`, `tipo`) VALUES
+(1, 609474291, 'maite@gmail.com', 'Maite García Correa', 'maii', NULL, '2015-12-09', '$2y$10$QLAC6M6SWzZhfx2kpXLGBOoyVbmhcnSaDhOB3aYYnAki33PEtYa.i', '68332d35ce067_carahellokitty.png', 'Genética,Síntomas crónicos,Diagnóstico,Tratamientos,Investigación', 'usu'),
+(2, 678935262, 'eri@gmail.com', 'Erica', 'Eriiiica', NULL, '2015-12-02', '$2y$10$/3pdB6n1U63uR.HC9WqVa.eVcSUnzF0NmGA6KfuidO0NpdLDmjs6e', '6833382972b1e_valorant.jpeg', 'Diagnóstico,Apoyo emocional,Investigación,Genética', 'usu');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `comentario`
+--
+ALTER TABLE `comentario`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_comentario_usuario` (`usuario`),
+  ADD KEY `fk_comentario_post` (`post`);
+
+--
+-- Indices de la tabla `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_likes_usuario` (`usuario`),
+  ADD KEY `fk_likes_post` (`post`);
+
+--
+-- Indices de la tabla `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_post_usuarios` (`usuario`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `telefono` (`telefono`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `comentario`
+--
+ALTER TABLE `comentario`
+  MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `post`
+--
+ALTER TABLE `post`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` bigint(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `comentario`
+--
+ALTER TABLE `comentario`
+  ADD CONSTRAINT `fk_comentario_post` FOREIGN KEY (`post`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_comentario_usuario` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `fk_likes_post` FOREIGN KEY (`post`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_likes_usuario` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `fk_post_usuarios` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
