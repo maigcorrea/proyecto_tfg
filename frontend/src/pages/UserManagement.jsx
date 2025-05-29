c3a55ba0f6c4dc08d029c40bd5b50dc46212caa9
unknown <anamaitegarciacorrea@gmail.com>
2025-05-29 14:30:58 +0200

    Me quedo mostrando todos los usuarios en el admin

--- frontend/src/pages/UserManagement.jsx
+++ frontend/src/pages/UserManagement.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getAllUsers } from '../services/userService';
 
 const UserManagement = () => {
+  const [users, setUsers] = useState(second);
+
+  useEffect(() => {
+    const obtenerUsuarios = async () =>{
+      const response = await getAllUsers();
+      console.log(response); 
+    }
+  }, []);
   return (
     <>
         <h1>PANEL USUARIOS</h1>
