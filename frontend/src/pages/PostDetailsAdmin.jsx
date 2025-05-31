2e9c50678ac1314892285240246ee460c533a5a0
Maite <anamaitegarciacorrea@gmail.com>
2025-05-31 23:55:29 +0200

    Botón de volver desde la página de detalles de un post hacia la página de todos los post (parte admin)

--- frontend/src/pages/PostDetailsAdmin.jsx
+++ frontend/src/pages/PostDetailsAdmin.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import DeletePostButton from '../components/AdminPanel/DeletePostButton'
 import PostDetails from '../components/AdminPanel/PostDetails'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+
 
 const PostDetailsAdmin = () => {
+    const navigate = useNavigate();
     const { id } = useParams();
   return (
     <>
-        <div className="p-4 bg-white block sm:flex items-center justify-end border-b border-gray-200 lg:mt-1.5 dark:bg-gray-800 dark:border-gray-700">
+        <div className="p-4 bg-white block sm:flex items-center justify-end gap-6 border-b border-gray-200 lg:mt-1.5 dark:bg-gray-800 dark:border-gray-700">
+            <button className='cursor-pointer' onClick={() => navigate("/admin/posts")}>Volver</button>
             <DeletePostButton postId={id}></DeletePostButton>
         </div>
         <PostDetails></PostDetails>
