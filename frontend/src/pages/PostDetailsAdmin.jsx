36cbf049e9f400d18d5ab9c5063ed828f77ee7b1
Maite <anamaitegarciacorrea@gmail.com>
2025-06-05 19:15:03 +0200

    PostDetails modificado

--- frontend/src/pages/PostDetailsAdmin.jsx
+++ frontend/src/pages/PostDetailsAdmin.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import DeletePostButton from '../components/AdminPanel/DeletePostButton'
 import PostDetails from '../components/AdminPanel/PostDetails'
 import { useNavigate, useParams } from 'react-router-dom'
+import { HiArrowUturnLeft } from "react-icons/hi2";
 
 
 const PostDetailsAdmin = () => {
@@ -10,7 +11,7 @@ const PostDetailsAdmin = () => {
   return (
     <>
         <div className="p-4 bg-white block sm:flex items-center justify-end gap-6 border-b border-gray-200 lg:mt-1.5 dark:bg-gray-800 dark:border-gray-700">
-            <button className='cursor-pointer' onClick={() => navigate("/admin/posts")}>Volver</button>
+            <button className='cursor-pointer text-gray-600 hover:text-gray-800 font-medium flex items-center' onClick={() => navigate("/admin/posts")}><HiArrowUturnLeft className='inline mr-2 size-5' />Volver</button>
             <DeletePostButton postId={id}></DeletePostButton>
         </div>
         <PostDetails></PostDetails>
