6cf8591ca265e46ea37be75b84c0eb283f52d330
Maite <anamaitegarciacorrea@gmail.com>
2025-06-08 15:25:24 +0200

    Footer mejorado

--- frontend/src/pages/Inicio.jsx
+++ frontend/src/pages/Inicio.jsx
@@ -81,7 +81,7 @@ const Inicio = () => {
     <section className='bg-white h-screen text-center'>
       <GridMotion items={items} gradientColor="black" />
         <div className="absolute bottom-50 left-20 bg-gradient-to-br z-10">
-          <h1 className="text-[100px] text-center text-[#9feeb5] font-bold">Singular Link</h1>
+          <h1 className="text-[100px] text-center text-[#a1c7c0] font-bold">Singular Link</h1>
         </div>
       </section>
 
