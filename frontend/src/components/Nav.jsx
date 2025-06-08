a0f86108828574b97b815c330e5af0b30868ff7b
Maite <anamaitegarciacorrea@gmail.com>
2025-06-08 22:21:44 +0200

    Avances

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -21,9 +21,9 @@ const Nav = () => {
       {userSession.tipo === "admin" ? (
         null
       ) : (
-        <nav class="bg-white/50 dark:bg-gray-900 fixed w-full  top-0 start-0 border-b border-gray-200 dark:border-gray-600 z-50 backdrop-blur-md bg-opacity-50 h-16">
-          <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <Link to="/"><span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Mi WEB</span></Link>
+        <nav class="bg-white/50 dark:bg-gray-900 fixed w-full  top-0 start-0 border-b border-gray-200 dark:border-gray-600 z-50 backdrop-blur-md bg-opacity-50 h-20">
+          <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 pt-0">
+            <Link to="/"><img src="/externalResources/logo.png" alt="" className='w-20 h-20'/></Link>
 
             <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
               {/* <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button> */}
@@ -47,7 +47,7 @@ const Nav = () => {
               class={`items-center justify-between w-full md:flex md:w-auto z-[100] md:order-1 ${menuOpen ? '' : 'hidden'} md:block`}
               id="navbar-sticky"
             >
-              <ul class="flex flex-col p-4 md:p-0 mt-4 z-[100] font-medium border rounded-full cursor-pointer md:space-x-8 rtl:space-x-reverse sm:bg-white md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <ul class="flex flex-col p-4 md:p-0 mt-4 z-[100] font-medium border rounded-xl bg-white/80 backdrop-blur-md cursor-pointer md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 sm:border-0">
                 <li className='hover:bg-[#a1c7c0] transition-colors duration-300 rounded-full p-[10px]'> 
                   <Link to="/" onClick={handleNavLinkClick}>Inicio</Link>
                 </li>
